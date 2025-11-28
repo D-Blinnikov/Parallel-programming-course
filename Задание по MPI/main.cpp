4c92c66d37fe747091d2e7190bb5ac1ec5a6525c
D-Blinnikov <dm.blinnikow@rambler.ru>
2025-11-28 15:27:41 +0300

    feat: description and comments

--- Задание по MPI/main.cpp
+++ Задание по MPI/main.cpp
@@ -1,6 +1,16 @@
 // номер 8
-// Паттерн: группы процессов и подкоммуникаторы (MPI_Comm_split)
-// Задача: посчитать суммы разных частей массива в двух группах (четные и нечетные ранги)
+// 1) Паттерн: 
+// Группы процессов и подкоммуникаторы
+// Разделение MPI_COMM_WORLD на подгруппы с помощью
+// MPI_Comm_split / MPI_Group_*.
+
+// 2) int (32-bit)
+
+// Задача: посчитать сумму массива чеез суммы разных частей массива 
+// в двух группах процессов (четные и нечетные ранги)
+
+// задача позволяетт показать, что можно создать несколько независимых процессов,
+// разбить их на групапы, группы процессов независимо работают на своими задачами
 
 
 #include <mpi.h>
@@ -11,22 +21,16 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    // дефолтное число процессов при запуске (указано в makefile) - 4
     MPI_Init(&argc, &argv);
-    // std::ios::sync_with_stdio(false);
-    // std::cout << std::unitbuf;
-
-    // std::cout << std::unitbuf;    // отключает буферизацию для cout
-    // // или так:
-    // std::ios::sync_with_stdio(false);
-    // std::cout << std::unitbuf;
 
     
-
     int world_rank, world_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    // чтобы не было повторения во всех процессах
+ 
+    // не дублируем
     if (world_rank == 0) {
         cout << "----------" << endl;
         cout << "Student number: 8" << endl;
@@ -35,21 +39,20 @@ int main(int argc, char* argv[]) {
         cout << "----------" << endl;
     }
 
-    // размер массива (для демонстрации)
     const int N = 1000000;
     vector<int> data;
 
-    // только rank 0 создаёт массив
+    // процесс 0 генерирует массив
     if (world_rank == 0) {
         data.resize(N);
         for (int i = 0; i < N; i++) data[i] = (i % 7) + 1;
     }
 
-    // шарим массив всем
+    // шарим массив всем процессам
     data.resize(N);
     MPI_Bcast(data.data(), N, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Создаем подкоммуникаторы: четные и нечетные ранги
+    // создаем подкоммуникаторы: четные и нечетные ранги
     int color = world_rank % 2;
     MPI_Comm subcomm;
     MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &subcomm);
@@ -58,18 +61,17 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(subcomm, &sub_rank);
     MPI_Comm_size(subcomm, &sub_size);
 
-    // каждый подкоммуникатор считает свою сумму,
+    // каждый процесс считает свою сумму части массива,
     // обрабатывая элементы с отступом в sub_size элементов
     long long local_sum = 0;
     for (int i = sub_rank; i < N; i += sub_size) {
         local_sum += data[i];
     }
 
-    // суммируем внутри группы
     long long group_sum = 0;
+    // суммируем независимо внутри групп, две группы процессов работают параллельно
     MPI_Reduce(&local_sum, &group_sum, 1, MPI_LONG_LONG, MPI_SUM, 0, subcomm);
 
-    // печатает результаты только главы групп (sub_rank == 0)
     if (sub_rank == 0) {
         cout << (color == 0 ? "[Group EVEN] " : "[Group ODD] ")
              << "sum = " << group_sum
